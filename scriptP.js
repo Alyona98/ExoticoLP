97c77fd6c5f1ffa10bb58d3c88174d9f212c48ef
Alyona98 <moshkinaalyona98@mail.ru>
2021-03-30 13:01:51 +0700

    add

---
+++ scriptP.js
@@ -0,0 +1,110 @@
+
+jQuery(function($) {
+	"use strict";
+
+//Адаптивность
+if ($('.fa-bars').length) {
+	$('.fa-bars').on('click', function(){
+		$('.header_top .inner_header nav > ul').show();
+		$(this).hide();
+		$('.fa-times').show();
+	});
+	$('.fa-times').on('click', function(){
+		$('.header_top .inner_header nav > ul').hide();
+		$(this).hide();
+		$('.fa-bars').show();
+	});
+}
+//Слайдер
+if ($('.home_slider').length) {
+	$('.home_slider').slick({
+		vertical: true,
+		arrows: false,
+		dots: true,
+	});
+}
+//.tweet_slider
+if ($('.content_slider_tweeter').length) {
+	$('.content_slider_tweeter').slick({
+		vertical: true,
+		appendArrows: $('.tweet_slider .arrows'),
+		prevArrow:"<span class=\"top\"><i class=\"fas fa-arrow-alt-circle-up\"></i></span>",
+		nextArrow:"<span class=\"down\"><i class=\"fas fa-arrow-alt-circle-down\"></i></span>"
+	});
+}
+//карусель-слайдер магазина
+if ($('.shop_carousel').length) {
+	$('.shop_carousel').slick({
+		infinite: true,
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		//отключение кнопок по умолчанию:
+		appendArrows: $('.shop_slider_navigation'),
+		prevArrow:"<div class=\"left\"><i class=\"fas fa-arrow-left\"></i></div>",
+		nextArrow:"<div class=\"right\"><i class=\"fas fa-arrow-right\"></i></div>",
+		//Адаптивность
+		responsive: [
+		    {
+		    	breakpoint: 420,
+		    	settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+		    	}
+		    }
+  		]
+	});
+}
+//Слайдер авторов блога
+if ($('.testimonial_carousel').length) {
+	$('.testimonial_carousel').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		//отключение кнопок по умолчанию:
+		appendArrows: $('.testimonial_slider_box'),
+		prevArrow:"<div class=\"left_arrow\"><div class=\"left\"><i class=\"fas fa-arrow-left\"></i></div></div>",
+		nextArrow:"<div class=\"right_arrow\"><div class=\"right\"><i class=\"fas fa-arrow-right\"></i></div></div>",
+		//Адаптивность
+		responsive: [
+		    {
+		    	breakpoint: 420,
+		    	settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+		    	}
+		    }
+  		]
+	});
+
+	$('.testimonial_carousel').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+	  var tabs_container = $('.testimonial_tab_conteiner');
+	  tabs_container.find($('.tab-item')).hide();
+	  tabs_container.find($('.tab_' + nextSlide)).show();
+	});
+}
+
+if ($('.open_video').length) {
+	$('.open_video').magnificPopup({
+		disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+	});
+}
+//Картинки блога (правый блок)
+if ($('.testy_gallery').length) {
+	$('.testy_gallery').magnificPopup({
+		type: 'image',
+          closeOnContentClick: true,
+          mainClass: 'mfp-img-mobile',
+          image: {
+            verticalFit: true
+          }
+          
+	});
+}
+
+})
